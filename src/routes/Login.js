3800d89421db063201743bd9ce3eecb72fce9ecc
aboublini <p19117@unipi.gr>
2023-11-21 17:37:26 +0200

    fetch from api

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -1,15 +1,56 @@
+import { useState, useEffect } from 'react';
 import './LoginStyle.css';
 
 const Login = () => {
+
+    // States for username, password and users retrieved from the mock API
+    const [username, setUsername] = useState();
+    const [password, setPassword] = useState();
+    const [users, setUsers] = useState([]);
+
+    // Retrieve user data from API
+    useEffect(
+        () => {
+          const fetchData = async () => {
+            const response = await fetch('https://655b7080ab37729791a91da3.mockapi.io/users/users');
+            const users = await response.json();
+            setUsers(users);
+            console.log(users);
+          };
+    
+          fetchData();
+        },
+        []
+      );
+
+    
+    // Handle changes in inputs
+    const handleUsernameChange = (e) => {
+        setUsername(e.target.value);
+        console.log(e.target.value);
+    }
+  
+    const handlePasswordChange = (e) => {
+      setPassword(e.target.value);
+      console.log(e.target.value !== "");
+    }
+
+    const handleLogin = (e) => {
+        // e.preventDefault();
+    
+        alert("Clicked!!");
+      }
+
     return (
         <div classname="container">
             <form className="login-form">
                     <label>Username</label>
-                    <input type="text" name="username" required/>
-                    <label>Password</label>
-                    <input type="password" name="password" required/>
+                    <input type="text" name="username" value={username} onChange={handleUsernameChange} required/>
 
-                    <button className="btn">Login</button>
+                    <label>Password</label>
+                    <input type="password" name="password" value={password} onChange={handlePasswordChange} required/>
+
+                    <button className="btn" disabled={username === "" && password === ""} onClick={handleLogin}>Login</button>
 
                     <div>
                         <input type="checkbox" id="lg" name="lg-check"/>
