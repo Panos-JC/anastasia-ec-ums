3662707fffab3a3381d29e4584b18c778e425300
aboublini <aboublini@gmail.com>
2023-11-24 18:11:09 +0200

    prettier tool changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import {Routes, Route} from 'react-router-dom';
-import Login from './routes/Login';
+import { Routes, Route } from "react-router-dom";
+import Login from "./routes/Login";
 
 function App() {
   return (
     <Routes>
-        <Route index element={<Login />} />
-        <Route path='/authentication' element={<Login/>}/>
+      <Route index element={<Login />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
-);
+  );
 }
 
 export default App;
