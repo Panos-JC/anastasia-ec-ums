5817e11e08cc86d76e49761af6dd05bf81d4f67b
aboublini <aboublini@gmail.com>
2023-12-20 14:34:48 +0200

    Update user data - API POST

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -5,6 +5,7 @@ import { useState, useEffect } from "react";
 
 const Home = () => {
   const [userData, setUserData] = useState({
+    id: "",
     username: "",
     password: "",
     fullName: "",
@@ -12,16 +13,26 @@ const Home = () => {
     role: "",
   });
   const [editable, setEditable] = useState(false);
-  const [savedPassword, setSavedPassword] = useState(localStorage.getItem("password"));
-  const [savedUsername, setSavedUsername] = useState(localStorage.getItem("username"));
+  const [savedPassword, setSavedPassword] = useState(
+    localStorage.getItem("password")
+  );
+  const [savedUsername, setSavedUsername] = useState(
+    localStorage.getItem("username")
+  );
 
   // Inputs
   const [passInput, setPassInput] = useState("");
   const [passConfInput, setPassConfInput] = useState("");
   const [nameInput, setNameInput] = useState("");
   const [ageInput, setAgeInput] = useState(0);
-
-
+  const [backup, setBackup] = useState({
+    id: "",
+    username: "",
+    password: "",
+    fullName: "",
+    age: 0,
+    role: "",
+  });
 
   // Retrieve user data from API
   useEffect(() => {
@@ -32,60 +43,71 @@ const Home = () => {
       const user = await response.json();
 
       // Find logged in user from saved credentials
-      setUserData(user.find(
-        (user) =>
-          user.username === savedUsername && user.password === savedPassword
-      ));
-      console.log(userData)
+      setUserData(
+        user.find(
+          (user) =>
+            user.username === savedUsername && user.password === savedPassword
+        )
+      );
+
+      // console.log(userData);
+      setBackup(
+        user.find(
+          (user) =>
+            user.username === savedUsername && user.password === savedPassword
+        )
+      );
+
+      console.log(userData);
     };
 
     fetchData();
-
-    
-
   }, []);
 
   // Handle edit button when clicked
   const handleEditCancel = (e) => {
+    // Toggle editable
     setEditable(!editable);
-    if (editable === false) {
-      // Cancel
-      setUserData({
-        username: userData.username,
-        password: userData.password,
-        fullName: userData.fullName,
-        age: userData.age,
-        role: userData.role,
-      });
-    }
+
+    // Cancel: Always set backup data (previous data from fetch)
+    setUserData({
+      id: backup.id,
+      username: backup.username,
+      password: backup.password,
+      fullName: backup.fullName,
+      age: backup.age,
+      role: backup.role,
+    });
+
     e.preventDefault();
   };
-
 
   // Input changes
   const handlePassInputChange = (e) => {
     setPassInput(e.target.value);
-    console.log(e.target.value !== "");
+    setUserData({ ...userData, password: e.target.value });
   };
 
   const handleConfPassInputChange = (e) => {
     setPassConfInput(e.target.value);
-    console.log(e.target.value !== "");
   };
 
   const handleNameInputChange = (e) => {
     setNameInput(e.target.value);
-    console.log(e.target.value !== "");
+    setUserData({ ...userData, fullName: e.target.value });
   };
 
   const handleAgeInputChange = (e) => {
     setAgeInput(e.target.value);
-    console.log(e.target.value !== "");
+    setUserData({ ...userData, age: e.target.value });
   };
-
 
   // Handle save button
   const handleSave = async (e) => {
+    console.log(userData);
+    // if (userData.password !== passConfInput) {
+    //   alert("Password is not the same!");
+    // } else {
     const response = await fetch(
       "https://655b7080ab37729791a91da3.mockapi.io/users/users/" + userData.id,
       {
@@ -93,13 +115,12 @@ const Home = () => {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({
-          fullName: nameInput,
-          password: passInput,
-          age: ageInput
-        }),
-      });
-  }
+        body: JSON.stringify(userData),
+      }
+    );
+    setEditable(editable);
+    // }
+  };
 
   return (
     <div className="home-container">
@@ -138,7 +159,6 @@ const Home = () => {
               <input
                 type="password"
                 name="conf-password"
-                value={userData.password}
                 disabled={!editable}
                 onChange={handleConfPassInputChange}
                 required
@@ -181,7 +201,11 @@ const Home = () => {
             <button className="btn-home" onClick={handleEditCancel}>
               {editable ? "Cancel" : "Edit"}
             </button>
-            <button className="btn-home" disabled={!editable} onClick={handleSave}>
+            <button
+              className="btn-home"
+              disabled={!editable}
+              onClick={handleSave}
+            >
               Save
             </button>
           </div>
