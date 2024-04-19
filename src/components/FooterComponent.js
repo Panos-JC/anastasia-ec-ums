ac5b99a82fc1e11f5dc430f445291f6ca2042bea
aboublini <aboublini@gmail.com>
2024-04-19 14:10:56 +0300

    footer and grid layout

---
+++ src/components/FooterComponent.js
@@ -0,0 +1,10 @@
+import "./FooterStyle.css";
+const FooterComponent = () => {
+  return (
+    <p className="foo">
+      © 2024 Random Company | random.company@mail.com | All rights reserved
+    </p>
+  );
+};
+
+export default FooterComponent;
--- src/layouts/Layout.js
+++ src/layouts/Layout.js
@@ -1,11 +1,14 @@
 import React from "react";
 import NavbarComponent from "../components/NavbarComponent";
+import FooterComponent from "../components/FooterComponent";
+import "./LayoutStyle.css";
 
 const Layout = ({ children }) => {
   return (
     <div className="grid">
       <NavbarComponent />
       {children}
+      <FooterComponent />
     </div>
   );
 };
